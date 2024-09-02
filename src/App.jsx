e051b92c29a74101a626eb6103f2b1caa88d8f6b
An1rudha07 <anirudhaupadhyay7107@gmail.com>
2024-09-03 02:51:10 +0530

    State mangement

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <button> HOME </button>
+
+    <button> MY NETWORK ()  </button>
+    <button> JOBS() </button>
+    <button> MESSAGING() </button>
+    <button> NOTIFICAION() </button>
+    
+    <button> ME </button>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/atom.js
@@ -0,0 +1,19 @@
+import { atom } from "recoil";
+
+
+export const networkAtom = atom({
+    key : "networkAtom",
+    default : 102
+})
+export const jobsAtom = atom({
+    key : "networkAtom",
+    default : 102
+})
+export const notificationAtom = atom({
+    key : "networkAtom",
+    default : 102
+})
+export const messagingAtom = atom({
+    key : "networkAtom",
+    default : 102
+})
